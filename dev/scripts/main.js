11b0a2f878d36c9fbc475ea53037fcac487e30ed
Steven Dixon <stdixon@atlanta.cgsinc.com>
2018-08-02 11:16:03 -0400

    vue up and running

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,63 @@
 (function(){
-    
+
+    fetch('./posts/posts.json').then(res => res.json())
+    .then(data => console.log(data));
+    const List = {
+        template: '#list-template',
+        data: () => ({
+            posts: []
+        }),
+        mounted() {
+            this.getPosts();
+        },
+        methods: {
+            getPosts() {
+                fetch('./posts/posts.json').then(res => res.json())
+                .then(data => this.posts = data._Entries).catch(error => {
+                    console.log(error);
+                })
+            }
+        }
+    };
+
+    // Post component
+    const Post = {
+        template: '#post-template',
+        data: () => ({
+            post: null
+        }),
+        mounted() {
+            this.getPosts();
+        },
+        methods: {
+            getPosts() {
+                let id = this.$route.params.id;
+                console.log(id)
+                fetch('./posts/posts.json').then(res => res.json())
+                .then(data => this.post = data._Entries.filter(item => item.Id === id)[0]).catch(error => {
+                    console.log(error);
+                })
+            }
+        }
+    };
+
+    // Create vue router
+    var router = new VueRouter({
+        routes: [
+            {
+                name: 'homepage',
+                path: '/',
+                component: List
+            }, {
+                name: 'post',
+                path: '/:id',
+                component: Post
+            }
+        ]
+    });
+
+    // Create vue instance with our router, and mount onto #app
+    var vue = new Vue({router}); 
+    var app = vue.$mount('#app'); 
+
 })()
